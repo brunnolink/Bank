8c58c7740ec286c898e19b85bcc10080b46f928c
brunno <brunnolinker@gmail.com>
2024-05-24 15:37:56 -0300

    Implementação de domínio

---
+++ src/main/java/project/api/devbank/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package project.api.devbank.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.api.devbank.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
